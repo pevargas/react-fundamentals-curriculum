b8e63343704d013158a7335e39be8d16e2de2156
Patrick Vargas <patrick@yesthisispatrick.com>
2016-09-16 22:17:19 -0400

    Add a basic navbar to main container

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,8 +2,15 @@ var React = require('react')
 
 function Main (props) {
   return (
-    <div className='container'>
-      {props.children}
+    <div className='main-container'>
+      <div className='navbar navbar-default'>
+        <div className='navbar-header'>
+          <a className='navbar-brand'>React Fundamentals Cirriculum App</a>
+        </div>
+      </div>
+      <div className='container'>
+        {props.children}
+      </div>
     </div>
   )
 }
